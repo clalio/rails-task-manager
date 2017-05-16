384c3eaf388506f55452f271892d09568020a308
clara elig <clara.elig@gmail.com>
2017-05-16 12:42:38 +0200

    first exo done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "dormir", details: "tot")
+Task.create(name: "manger", details: "à midi")
